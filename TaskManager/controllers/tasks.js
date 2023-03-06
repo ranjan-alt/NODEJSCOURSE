e02b537e4942ac3ea41aa0e11e5e8e5bc30a64ac
Ranjan Kumar <67799181+ranjan-alt@users.noreply.github.com>
2023-03-06 18:02:47 +0000

    send id

--- TaskManager/controllers/tasks.js
+++ TaskManager/controllers/tasks.js
@@ -7,7 +7,7 @@ const createTask = (req, res) => {
 };
 
 const getTask = (req, res) => {
-  res.send("get tASK");
+  res.json({ id: req.params.id });
 };
 const updateTask = (req, res) => {
   res.send("update tASK");
