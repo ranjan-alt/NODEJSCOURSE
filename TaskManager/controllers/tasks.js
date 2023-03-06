0880a0338d58aca06629fc9d52a649d07f8044c7
Ranjan Kumar <67799181+ranjan-alt@users.noreply.github.com>
2023-03-06 19:58:47 +0000

    validation error

--- TaskManager/controllers/tasks.js
+++ TaskManager/controllers/tasks.js
@@ -1,12 +1,16 @@
-const Task = require("../modals/Task")
+const Task = require("../modals/Task");
 
 const getAllTask = (req, res) => {
   res.send("all items");
 };
 
-const createTask = async(req, res) => {
-  const task = await Task.create(req.body)
-  res.status(201).json({task});
+const createTask = async (req, res) => {
+  try {
+    const task = await Task.create(req.body);
+    res.status(201).json({ task });
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
 };
 
 const getTask = (req, res) => {
