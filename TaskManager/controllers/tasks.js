c16754145c935150d428d63cf81a1cab5d1101a0
Ranjan Kumar <67799181+ranjan-alt@users.noreply.github.com>
2023-03-07 09:36:07 +0000

    getsingletask

--- TaskManager/controllers/tasks.js
+++ TaskManager/controllers/tasks.js
@@ -18,8 +18,19 @@ const createTask = async (req, res) => {
   }
 };
 
-const getTask = (req, res) => {
-  res.json({ id: req.params.id });
+const getTask = async (req, res) => {
+  try {
+    const { id: taskId } = req.params;
+    const task = await Task.findOne({ _id: taskId });
+
+    if (!task) {
+      return res.status(400).json({ msg: `no task for this id : ${taskId}` });
+    }
+    // res.status(200).json({ id: req.params.id });
+    res.status(200).json({ task });
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
 };
 const updateTask = (req, res) => {
   res.send("update tASK");
