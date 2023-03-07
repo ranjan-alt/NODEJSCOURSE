db65da4da7cb916b027df910c65d0c1c3636a4f3
Ranjan Kumar <67799181+ranjan-alt@users.noreply.github.com>
2023-03-07 10:03:15 +0000

    update task complete

--- TaskManager/controllers/tasks.js
+++ TaskManager/controllers/tasks.js
@@ -32,13 +32,20 @@ const getTask = async (req, res) => {
     res.status(500).json({ msg: error });
   }
 };
-const updateTask = (req, res) => {
+const updateTask = async (req, res) => {
   try {
     const { id: taskId } = req.params;
-
-    res.status(200).json({ id: taskId, data: req.body });
-  } catch (error) {}
-  res.send("update tASK");
+    const task = await Task.findOneAndUpdate({ _id: taskId }, req.body, {
+      new: true,
+      runValidators: true,
+    });
+    if (!task) {
+      return res.status(400).json({ msg: `no task for this id : ${taskId}` });
+    }
+    res.status(200).json({ task });
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
 };
 
 const deleteTask = async (req, res) => {
